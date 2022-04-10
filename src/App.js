2688b7a08150f82db7b7d3a92cd5fb1f204f23fc
Thail3 <tanai.saeng@gmail.com>
2022-04-10 19:04:46 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { useState } from "react";
+import { FaEdit, FaTrash } from "react-icons/fa";
+import "./App.css";
+
+const getLocalStorage = () => {
+  let list = localStorage.getItem("list");
+  if (list) {
+    return (list = JSON.parse(localStorage.getItem("list")));
+  } else {
+    return [];
+  }
+};
 
 function App() {
+  const [name, setName] = useState("");
+  const [list, setList] = useState(getLocalStorage());
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (name && isEditing) {
+      setList(
+        list.map((item) => {
+          if (item.id === editID) {
+            return { ...item, title: name };
+          }
+          return item;
+        })
+      );
+      setName("");
+      setEditID(null);
+      setIsEditing(false);
+    } else {
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      console.log(newItem);
+
+      setList([...list, newItem]);
+      setName("");
+    }
+  };
+
+  const clearList = () => {
+    setList([]);
+  };
+
+  const removeItem = (id) => {
+    setList(list.filter((item) => item.id !== id));
+  };
+
+  const editItem = (id) => {
+    const spacificItem = list.find((item) => item.id === id);
+    setIsEditing(true);
+    setEditID(id);
+    setName(spacificItem.title);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={handleSubmit}>
+          <p>TODO LIST</p>
+          <div className="input">
+            <input
+              type="text"
+              placeholder="Please input a items"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            ></input>
+            <button>{isEditing ? "Edit" : "Submit"}</button>
+          </div>
+        </form>
       </header>
+
+      <main className="main-list">
+        {list.map((item) => {
+          const { id, title } = item;
+          return (
+            <article key={id} className="container">
+              <p>{title}</p>
+              <div
+                className="btn-container"
+                style={{ display: "flex", alignItems: "center" }}
+              >
+                <button className="btn-edit" onClick={() => editItem(id)}>
+                  <FaEdit />
+                </button>
+                <button className="btn-delete" onClick={() => removeItem(id)}>
+                  <FaTrash />
+                </button>
+              </div>
+            </article>
+          );
+        })}
+      </main>
+
+      {list.length > 0 && (
+        <footer>
+          <button className="clear-btn" onClick={clearList}>
+            clear items
+          </button>
+        </footer>
+      )}
     </div>
   );
 }
